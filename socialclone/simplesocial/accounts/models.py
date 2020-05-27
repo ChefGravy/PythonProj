5043af69c7b2aec40cb27935403a5d52d61420c4
ChefGravy <h82w84u@gmail.com>
2020-05-27 12:55:52 -0700

    social clone

---
+++ socialclone/simplesocial/accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+#many authorization accounts are with the auth module
+from django.contrib import auth
+
+# Create your models here.
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        #string representation of a user
+        return "@{}".format(self.username)
