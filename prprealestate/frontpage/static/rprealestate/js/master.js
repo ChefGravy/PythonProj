38571d8a35795cfcd2783cdbad560b1b0596122e
ChefGravy <h82w84u@gmail.com>
2020-08-25 19:34:01 -0700

    first commit

--- prprealestate/frontpage/static/rprealestate/js/master.js
+++ prprealestate/frontpage/static/rprealestate/js/master.js
@@ -1,20 +1,17 @@
-//Phone number
-function sendMail(value) {
-  var link = 'mailto: h82w84u@gmail.com'
-  + '$subject=' + encodeURIComponent(document.getElementById('ph-number').value);
-  window.location.href = link;
-}
+// //Phone number
+// function sendMail(value) {
+//   var link = 'mailto: h82w84u@gmail.com'
+//   + '$subject=' + encodeURIComponent(document.getElementById('ph-number').value);
+//   window.location.href = link;
+// }
 
 const renderPh = () => {
   const markup =
     `
-    <div class="input-group col-sm-3">
-      <input type="text" class="form-control" id='ph-number' placeholder="Ten Digit Phone#" aria-label="Phone number" aria-describedby="basic-addon2">
-      <div class="input-group-append">
-        <button onclick='sendMail(); return flase' class="btn btn-info" type="button">Call Me</button>
-      </div>
-    </div>
-  `
+      <button id='free-consultation' class="btn btn-info" disabled><b>GET FREE CONSULTATION</b></button>
+      <p><span style="font-family: 'Baloo Tamma 2', cursive; font-size: 20px; color:black;"><b>Give me a call @ (206) 601-2325.</b></span>
+    `
+
   document.getElementById('index-container').insertAdjacentHTML('beforeend', markup);
 };
 
@@ -23,17 +20,17 @@ document.getElementById('free-consultation').addEventListener('click', () => {
   document.getElementById('free-consultation').remove();
     renderPh();
 });
-
-//validate number and e-mail
-(function () {
-  const valuePh = document.getElementById('ph-number');
-  if(valuePh) {
-    valuePh.addEventListener('click', () => {
-      if(valuePh.value.length === 10 && !isNaN(valuePh.value)){
-        sendMail(valuePh.value);
-      } else {
-        alert('Ooops, something went wrong. Please double check your entry and try again!')
-      }
-    });
-  }
-})();
+//
+// //validate number and e-mail
+// (function () {
+//   const valuePh = document.getElementById('ph-number');
+//   if(valuePh) {
+//     valuePh.addEventListener('click', () => {
+//       if(valuePh.value.length === 10 && !isNaN(valuePh.value)){
+//         sendMail(valuePh.value);
+//       } else {
+//         alert('Ooops, something went wrong. Please double check your entry and try again!')
+//       }
+//     });
+//   }
+// })();
