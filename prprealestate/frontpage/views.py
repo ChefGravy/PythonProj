c031c32f69802802da374912cd284eea1fdb3a30
ChefGravy <h82w84u@gmail.com>
2020-09-02 15:05:16 -0700

    first commit

--- prprealestate/frontpage/views.py
+++ prprealestate/frontpage/views.py
@@ -1,4 +1,9 @@
+
+#allows us to send email with our setting
+from django.core.mail import send_mail
+from django.conf import settings
 from django.shortcuts import render
+from django.contrib import messages
 # from django.db import models
 from frontpage import models
 from frontpage.models import ContactForm
@@ -21,6 +26,14 @@ def contact(request):
                 subject = form.cleaned_data['subject']
                 body = form.cleaned_data['body']
                 form.save()
+                #send_mail(subject, message, from_email, fail_silently=True)
+                subject_email = 'PRP Real Estate'
+                message_email = 'Welcome and thank you for your e-mail! I will be in touch soon.'
+                from_email = settings.EMAIL_HOST_USER
+                #this e-mail is for owner with all the details
+                send_mail(subject, 'Name: ' + name + '\nEmail of sender: ' + email + '\nCategory: ' + category + '\nSubject: ' + subject + '\nBody: '+ body, email, [from_email], fail_silently=True)
+                #this e-mail is for user
+                send_mail(subject_email, message_email, 'ruslan@uw.edu',[email], fail_silently=True)
 
         form = ContactForm()
         return render(request, 'frontpage/contactform.html', {'form':form})
---
+++ prprealestate/rprealestate/email_info.py
@@ -0,0 +1,8 @@
+#https://myaccount.google.com/lesssecureapps
+#https://myaccount.google.com/apppasswords
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'h82w84u@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'pfzmkcjzzjoiggix'
